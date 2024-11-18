250158407380190858c39c70e9af98166b30a94f
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2024-11-18 15:35:27 +0900

    carousel and nav sticky

--- app.vue
+++ app.vue
@@ -7,4 +7,30 @@
 </template>
 
 <script setup>
+import { useHead } from 'nuxt/app'
+
+useHead({
+  script: [
+    {
+      src: '/js/animation.js',
+      type: 'text/javascript',
+      defer: true
+    },
+    {
+      src: '/js/templatemo-custom.js',
+      type: 'text/javascript',
+      defer: true
+    },
+    {
+      src: '/js/imagesloaded.js',
+      type: 'text/javascript',
+      defer: true
+    },
+    {
+      src: '/js/owl-carousel.js',
+      type: 'text/javascript',
+      defer: true
+    },
+  ]
+})
 </script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -26,7 +26,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </header>
 </template>
 
 <script lang="ts">
