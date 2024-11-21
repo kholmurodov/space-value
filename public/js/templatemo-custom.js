a1e76ef70dfdaa08b9b7ac5d51bf8ea22d89b010
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2024-11-21 10:10:53 +0900

    fix

--- public/js/templatemo-custom.js
+++ public/js/templatemo-custom.js
@@ -98,7 +98,7 @@
     $('.nav a').each(function () {
       var currLink = $(this);
       var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      if (refElement && refElement.position() && refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
         $('.nav ul li a').removeClass("active");
         currLink.addClass("active");
       }
