97997e0348655a0f2a77f7ae8d4b9059b5700c4d
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2024-11-26 19:02:43 +0900

    install CKEditor

---
+++ components/CKeditor.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container mx-auto max-w-4xl my-8">
+        <section>
+            <div v-if="editor"
+                class="btn d-flex align-items-center flex-fill flex-wrap g-col-4 border border-primary p-4">
+                <ckeditor :editor="editor" />
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup>
+const { $ckeditor } = useNuxtApp();
+
+const editor = $ckeditor.classicEditor;
+
+</script>
---
+++ pages/blogs/edit/[id]/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <div class="container our-blog section pt-0 ">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Please Enter a Title" aria-label="Title"
+                    aria-describedby="basic-addon1" v-model="blog.title">
+            </div>
+
+            <div class="input-group mb-3">
+                <input type="file" class="form-control" id="inputGroupFile02">
+                <label class="input-group-text" for="inputGroupFile02">Upload</label>
+            </div>
+            <CKeditor />
+            <button type="button" class="btn btn-success">{{ $t("blogs.save") }}</button>
+        </div>
+    </main>
+</template>
+
+
+<script setup lang="ts">
+
+const route = useRoute();
+const { id } = route.params;
+
+const store = useBlogStore();
+const { getBlog } = store;
+const blog = getBlog(id as string);
+
+if (!blog) {
+    throw new Error("Blog Not Found");
+}
+
+
+
+</script>
