e8c4fe4fe7f090837c46046546df81ff8ac62071
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2025-03-04 11:38:00 +0900

    WIP

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -3,31 +3,17 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-4">
-          <div
-            class="left-image wow fadeIn"
-            data-wow-duration="1s"
-            data-wow-delay="0.2s"
-          >
-            <img
-              src="assets/images/about-left-image.png"
-              alt="person graphic"
-            />
+          <div class="left-image wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+            <img src="assets/images/about-left-image.png" alt="person graphic" />
           </div>
         </div>
         <div class="col-lg-8 align-self-center">
           <div class="services">
             <div class="row">
               <div class="col-lg-6">
-                <div
-                  class="item wow fadeIn"
-                  data-wow-duration="1s"
-                  data-wow-delay="0.5s"
-                >
+                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
                   <div class="icon">
-                    <img
-                      src="assets/images/service-icon-01.jpg"
-                      alt="reporting"
-                    />
+                    <img src="assets/images/service-icon-01.jpg" alt="reporting" />
                   </div>
                   <div class="right-text">
                     <h4>시스템 개발 및 유지보수</h4>
@@ -39,11 +25,7 @@
                 </div>
               </div>
               <div class="col-lg-6">
-                <div
-                  class="item wow fadeIn"
-                  data-wow-duration="1s"
-                  data-wow-delay="0.7s"
-                >
+                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.7s">
                   <div class="icon">
                     <img src="assets/images/service-icon-02.jpg" alt="" />
                   </div>
@@ -58,11 +40,7 @@
                 </div>
               </div>
               <div class="col-lg-6">
-                <div
-                  class="item wow fadeIn"
-                  data-wow-duration="1s"
-                  data-wow-delay="0.9s"
-                >
+                <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
                   <div class="icon">
                     <img src="assets/images/service-icon-03.jpg" alt="" />
                   </div>
@@ -76,11 +54,7 @@
                 </div>
               </div>
               <div class="col-lg-6">
-                <div
-                  class="item wow fadeIn"
-                  data-wow-duration="1s"
-                  data-wow-delay="1.1s"
-                >
+                <div class="item wow fadeIn w-100" data-wow-duration="1s" data-wow-delay="1.1s">
                   <div class="icon">
                     <img src="assets/images/service-icon-01.jpg" alt="" />
                   </div>
